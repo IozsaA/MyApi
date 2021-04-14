775198d9810f8a3b0774485ee210c41071f6717f
Iozsa Andrei <andrei.iozsa@student.upt.ro>
2021-04-14 21:13:33 +0300

    the API is working

--- main.py
+++ main.py
@@ -1,20 +1,73 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-drinks_data = {"palinca": {"grade": 52},
-                "vodka": {"grade": 40}}
 
+class DrinkModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    grade = db.Column(db.Integer, nullable=False)
+
+
+
+drinks_put_args = reqparse.RequestParser()
+drinks_put_args.add_argument("name", type=str, required=True)
+drinks_put_args.add_argument("grade", type=int, required=True)
+
+
+drinks_update_args= reqparse.RequestParser()
+drinks_update_args.add_argument("name", type=str)
+drinks_update_args.add_argument("grade", type=int)
+
+resource_fields = {
+    'id': fields.Integer,
+    'name':fields.String,
+    'grade':fields.String
+}
+
+drinks_data = {}
 class Drinks(Resource):
-    def get(self, name):
-        return drinks_data[name]
+    @marshal_with(resource_fields)
+    def get(self, drink_id):
+        result = DrinkModel.query.filter_by(id=drink_id).first()
+        if not result:
+            abort(404,message="Could not find this drink")
+        return result
 
-    def put(self, name):
-        return 
+    @marshal_with(resource_fields)
+    def put(self, drink_id):
+        args = drinks_update_args.parse_args()
+        result = DrinkModel.query.filter_by(id=drink_id).first()
+        if result:
+            abort(409,message="Drink already exists..")
+        drink = DrinkModel(id = drink_id, name=args['name'], grade = args['grade'])
+        db.session.add(drink)
+        db.session.commit()
+        return drink, 201
 
-api.add_resource(Drinks, "/drinks/<string:name>/")
+    @marshal_with(resource_fields)
+    def patch(self,drink_id):
+        args= drinks_put_args.parse_args()
+        result = DrinkModel.query.filter_by(id=drink_id).first()
+        if not result:
+            abort(404, message="Could not find this drink")
+        if args['name']:
+            DrinkModel.name = args['name']
+        if args['grade']:
+            DrinkModel.grade = args['grade']
+        db.session.commit()
+        return result
+
+    def delete(self, drink_id):
+        del drinks_data[drink_id]
+        return '', 204
+
+api.add_resource(Drinks, "/drink/<int:drink_id>")
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,5 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "drinks/palinca/")
-print(response.json())
+# data = [
+#     {"name": "palinca", "grade": 55},
+#     {"name": "whiskey", "grade": 40},
+#     {"name": "vodka", "grade": 40},
+#     {"name": "tequila", "grade": 36},
+#     {"name": "tatra-tea", "grade": 80},
+#     {"name": "absinthe", "grade": 65},
+# ]
+#
+# for i in range(len(data)):
+#     response = requests.put(BASE +"drink/" + str(i), data[i])
+#     print(response.json())
+# input()
+
+response = requests.patch(BASE + "drink/2", {"name":"tuica", "grade": 42})
+print(response)
